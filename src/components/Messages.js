cbfc48efd6c1cba659deec4f29dd523958a665e1
Ian <ianmcm.89@gmail.com>
2021-12-16 13:06:21 -0500

    completed assignment

---
+++ src/components/Messages.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Messages() {
+    return (
+      <div>
+        <h1>Messages:</h1>
+        <p>Message 1</p>
+        <p>Message 2</p>
+        <p>Message 3</p>
+      </div>
+    );
+  }
+
+  export default Messages;
